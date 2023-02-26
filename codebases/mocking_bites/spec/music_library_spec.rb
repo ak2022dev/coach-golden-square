3fcc2c79c105872471b8bb166c16defcc6f75706
Amir Khan <ak2022dev@gmail.com>
2023-02-26 16:54:13 +0000

    TDD pass further tests matching more than one title in music library

--- codebases/mocking_bites/spec/music_library_spec.rb
+++ codebases/mocking_bites/spec/music_library_spec.rb
@@ -51,5 +51,28 @@ RSpec.describe MusicLibrary do
     matches = music_lib.search("Heart")
     expect(matches).to be_empty
   end
+  it "finds more than one matching track" do
+    music_lib = MusicLibrary.new
+    track1 = Track.new("Love story", "Taylor Swift")
+    track2 = Track.new("Lipstick on Your Collar", "Connie Francis")
+    track3 = Track.new("Love", "Lana Del Ray")
+    music_lib.add(track1)
+    music_lib.add(track2)
+    music_lib.add(track3)
+    matches = music_lib.search("Love")
+    expect(matches).to include("Love story" => "Taylor Swift")
+    expect(matches).to include("Love" => "Lana Del Ray")
+  end
+  it "finds more than one matching track" do
+    music_lib = MusicLibrary.new
+    track1 = Track.new("Lipstick on Your Collar", "Connie Francis")
+    track2 = Track.new("Love story", "Taylor Swift")
+    track3 = Track.new("Love", "Lana Del Ray")
+    music_lib.add(track1)
+    music_lib.add(track2)
+    music_lib.add(track3)
+    matches = music_lib.search("Love")
+    expect(matches).to include("Love" => "Lana Del Ray")
+    expect(matches).to include("Love story" => "Taylor Swift")
+  end
 end
-
