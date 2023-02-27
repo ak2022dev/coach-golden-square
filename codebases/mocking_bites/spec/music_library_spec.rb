fb2ed97218289338caa0abd0383bda32e14c2c89
Amir Khan <ak2022dev@gmail.com>
2023-02-27 11:41:52 +0000

    TDD unit test MusicLibrary pass test to add a track using double

--- codebases/mocking_bites/spec/music_library_spec.rb
+++ codebases/mocking_bites/spec/music_library_spec.rb
@@ -1,6 +1,5 @@
 # file: spec/music_library_spec.rb
 
-require "track"
 require "music_library"
 
 
@@ -9,10 +8,9 @@ RSpec.describe MusicLibrary do
     music_lib = MusicLibrary.new
     expect(music_lib.all).to eq({})
   end
-  xit "adds a track" do
+  it "adds a track" do
     music_lib = MusicLibrary.new
-    track = Track.new("Harbour Lights","The Platters")
-    music_lib.add(track)
+    music_lib.add(double(:fake_track, title: "Harbour Lights", artist: "The Platters"))
     expect(music_lib.all).to include("Harbour Lights" => "The Platters")
   end
   xit "adds more than one track" do
