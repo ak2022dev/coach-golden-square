6284ee809a9b841a9210f5891198ee47d16dc532
Amir Khan <ak2022dev@gmail.com>
2023-03-04 11:53:45 +0000

    TDD refactor unit tests all pass for music library to mock track

--- codebases/mocking_bites/spec/music_library_spec.rb
+++ codebases/mocking_bites/spec/music_library_spec.rb
@@ -27,93 +27,91 @@ RSpec.describe MusicLibrary do
     music_lib = MusicLibrary.new
     track1 = double(:fake_track1, title: "Harbour Lights", artist: "The Platters")
     track2 = double(:fake_track2, title: "Lipstick on Your Collar", artist: "Connie Francis")
-    expect(track2).to receive(:matches?).with("Harbour").and_return(false)
     expect(track1).to receive(:matches?).with("Harbour").and_return(true)
+    expect(track2).to receive(:matches?).with("Harbour").and_return(false)
     music_lib.add(track1)
     music_lib.add(track2)
     matches = music_lib.search("Harbour")
     expect(matches).to include(track1)
     expect(matches).not_to include(track2)
   end
-  xit "finds one matching track" do
+  it "finds one matching track" do
     music_lib = MusicLibrary.new
     track1 = double(:fake_track1, title: "Harbour Lights", artist: "The Platters")
     track2 = double(:fake_track2, title: "Lipstick on Your Collar", artist: "Connie Francis")
     expect(track1).to receive(:matches?).with("Lipstick").and_return(false)
     expect(track2).to receive(:matches?).with("Lipstick").and_return(true)    
     music_lib.add(track1)
-    p "music_lib is #{music_lib}"
     music_lib.add(track2)
-    p "music_lib is #{music_lib}"
     matches = music_lib.search("Lipstick")
-    p "matches is #{matches}"
-    expect(matches).to include("Lipstick on Your Collar" => "Connie Francis")
-    expect(matches).not_to include("Harbour Lights" => "The Platters")
+    expect(matches).to include(track2)
+    expect(matches).not_to include(track1)
   end
-  xit "reports no matching track or artist" do
+  it "reports no matching track or artist" do
     music_lib = MusicLibrary.new
-    track1 = Track.new("Harbour Lights", "The Platters")
-    track2 = Track.new("Lipstick on Your Collar", "Connie Francis")
+    track1 = double(:fake_track1, title: "Harbour Lights", artist: "The Platters")
+    track2 = double(:fake_track2, title: "Lipstick on Your Collar", artist: "Connie Francis")
+    expect(track1).to receive(:matches?).with("Heart").and_return(false)
+    expect(track2).to receive(:matches?).with("Heart").and_return(false)    
     music_lib.add(track1)
     music_lib.add(track2)
     matches = music_lib.search("Heart")
     expect(matches).to be_empty
   end
-  xit "finds more than one matching track" do
+  it "finds more than one matching track" do
     music_lib = MusicLibrary.new
-    track1 = Track.new("Love story", "Taylor Swift")
-    track2 = Track.new("Lipstick on Your Collar", "Connie Francis")
-    track3 = Track.new("Love", "Lana Del Ray")
+    track1 = double(:fake_track1, title: "Love story", artist: "Taylor Swift")
+    track2 = double(:fake_track2, title: "Lipstick on Your Collar", artist: "Connie Francis")
+    track3 = double(:fake_track3, title: "Love", artist: "Lana Del Ray")
+    expect(track1).to receive(:matches?).with("Love").and_return(true)
+    expect(track2).to receive(:matches?).with("Love").and_return(false)    
+    expect(track3).to receive(:matches?).with("Love").and_return(true)
     music_lib.add(track1)
     music_lib.add(track2)
     music_lib.add(track3)
     matches = music_lib.search("Love")
-    expect(matches).to include("Love story" => "Taylor Swift")
-    expect(matches).to include("Love" => "Lana Del Ray")
+    expect(matches).to include(track1)
+    expect(matches).not_to include(track2)
+    expect(matches).to include(track3)
   end
-  xit "finds more than one matching track" do
+  it "finds one matching artist" do
     music_lib = MusicLibrary.new
-    track1 = Track.new("Lipstick on Your Collar", "Connie Francis")
-    track2 = Track.new("Love story", "Taylor Swift")
-    track3 = Track.new("Love", "Lana Del Ray")
-    music_lib.add(track1)
-    music_lib.add(track2)
-    music_lib.add(track3)
-    matches = music_lib.search("Love")
-    expect(matches).to include("Love" => "Lana Del Ray")
-    expect(matches).to include("Love story" => "Taylor Swift")
-  end
-  xit "finds one matching artist" do
-    music_lib = MusicLibrary.new
-    track1 = Track.new("Harbour Lights", "The Platters")
-    track2 = Track.new("Lipstick on Your Collar", "Connie Francis")
+    track1 = double(:fake_track1, title: "Harbour Lights", artist: "The Platters")
+    track2 = double(:fake_track2, title: "Lipstick on Your Collar", artist: "Connie Francis")
+    expect(track1).to receive(:matches?).with("Platters").and_return(true)
+    expect(track2).to receive(:matches?).with("Platters").and_return(false)    
     music_lib.add(track1)
     music_lib.add(track2)
     matches = music_lib.search("Platters")
-    expect(matches).to include("Harbour Lights" => "The Platters")
-    expect(matches).not_to include("Lipstick on Your Collar" => "Connie Francis")
+    expect(matches).to include(track1)
+    expect(matches).not_to include(track2)
   end
-  xit "finds one matching artist" do
+  it "finds one matching artist" do
     music_lib = MusicLibrary.new
-    track1 = Track.new("Harbour Lights", "The Platters")
-    track2 = Track.new("Lipstick on Your Collar", "Connie Francis")
+    track1 = double(:fake_track1, title: "Harbour Lights", artist: "The Platters")
+    track2 = double(:fake_track2, title: "Lipstick on Your Collar", artist: "Connie Francis")
+    expect(track1).to receive(:matches?).with("Connie").and_return(false)
+    expect(track2).to receive(:matches?).with("Connie").and_return(true)    
     music_lib.add(track1)
     music_lib.add(track2)
-    matches = music_lib.search("Connie Francis")
-    expect(matches).not_to include("Harbour Lights" => "The Platters")
-    expect(matches).to include("Lipstick on Your Collar" => "Connie Francis")
+    matches = music_lib.search("Connie")
+    expect(matches).not_to include(track1)
+    expect(matches).to include(track2)
   end
-  xit "finds more than one matching artist" do
+  it "finds more than one matching artist" do
     music_lib = MusicLibrary.new
-    track1 = Track.new("Lipstick on Your Collar", "Connie Francis")
-    track2 = Track.new("Heart of Glass", "Blondie")
-    track3 = Track.new("Sunday Girl", "Blondie")
+    track1 = double(:fake_track1, title: "Heart of Glass", artist: "Blondie")
+    track2 = double(:fake_track2, title: "Sunday Girl", artist: "Blondie")
+    track3 = double(:fake_track3, title: "Lipstick on Your Collar", artist: "Connie Francis")
+    expect(track1).to receive(:matches?).with("Blondie").and_return(true)
+    expect(track2).to receive(:matches?).with("Blondie").and_return(true)    
+    expect(track3).to receive(:matches?).with("Blondie").and_return(false)    
     music_lib.add(track1)
     music_lib.add(track2)
     music_lib.add(track3)
     matches = music_lib.search("Blondie")
-    expect(matches).to include("Heart of Glass" => "Blondie")
-    expect(matches).to include("Sunday Girl" => "Blondie")
-    expect(matches).not_to include("Lipstick on Your Collar" => "Connie Francis")
+    expect(matches).to include(track1)
+    expect(matches).to include(track2)
+    expect(matches).not_to include(track3)
   end
 end
