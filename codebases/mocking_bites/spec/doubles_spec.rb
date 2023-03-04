e3e0caa8111e448c329c5dac339eeef82332baf2
Amir Khan <ak2022dev@gmail.com>
2023-03-04 15:04:25 +0000

    TDD pass tests for additional challenge examples

--- codebases/mocking_bites/spec/doubles_spec.rb
+++ codebases/mocking_bites/spec/doubles_spec.rb
@@ -59,4 +59,20 @@ RSpec.describe "doubles" do
     fake_diary.add(double :diary_entry)
     expect(fake_diary.count_entries).to eq 2
   end
+
+  it "creates a sophisticated double" do
+    task_list = double(:fake_task_list)
+    task = double(:fake_task)
+
+    expect(task_list).to receive(:add).with(task)
+    expect(task_list).to receive(:list).and_return([task])
+    expect(task_list).to receive(:count).and_return(1)
+    expect(task_list).to receive(:clear).and_return(:success)
+
+    # Don't edit below
+    task_list.add(task)
+    expect(task_list.list).to eq [task]
+    expect(task_list.count).to eq 1
+    expect(task_list.clear).to eq :success
+  end
 end
