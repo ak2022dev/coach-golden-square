dfa6f0a2a5d537615e5bf87ee40841b8c64dceae
Amir Khan <ak2022dev@gmail.com>
2023-03-05 12:36:22 +0000

    Re-add secret_diary_spec.rb initially as copy of the integration test version but with disabled tests to refactor using mocking

--- codebases/mocking_bites/spec/secret_diary_spec.rb
+++ codebases/mocking_bites/spec/secret_diary_spec.rb
@@ -1,21 +1,21 @@
-# File spec/secret_diary_integration_spec.rb
+# File spec/secret_diary_spec.rb
 
 require "diary"
 require "secret_diary"
 
 RSpec.describe SecretDiary do
-  it "initially locks a diary" do
+  xit "initially locks a diary" do
     diary = Diary.new("My contents")
     secret_diary = SecretDiary.new(diary)
     expect{secret_diary.read}.to raise_error("Go away!")
   end
-  it "allows reading an unlocked diary" do
+  xit "allows reading an unlocked diary" do
     diary = Diary.new("My contents")
     secret_diary = SecretDiary.new(diary)
     secret_diary.unlock
     expect(secret_diary.read).to eq("My contents")
   end
-  it "locks an unlocked diary" do
+  xit "locks an unlocked diary" do
     diary = Diary.new("My contents")
     secret_diary = SecretDiary.new(diary)
     secret_diary.unlock
