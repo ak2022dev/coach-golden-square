483d3a960f579f7b03d1bf1057a795e86612018b
Amir Khan <ak2022dev@gmail.com>
2023-04-09 15:16:30 +0100

    Add InteractiveCalculator class and unit test for using terminal mocking

--- codebases/mocking_bites/lib/interactive_calculator.rb
+++ codebases/mocking_bites/lib/interactive_calculator.rb
@@ -1,2 +1,16 @@
 class InteractiveCalculator
+  def initialize(terminal)
+    @terminal = terminal
+  end
+  def run
+    @terminal.puts("Hello. I will subtract two numbers")
+    @terminal.puts("Please enter a number")
+    num1 = @terminal.gets()
+    num1 = num1.chomp.to_i
+    @terminal.puts("Please enter another number")
+    num2 = @terminal.gets()
+    num2 = num2.chomp.to_i
+    @terminal.puts("Here is your result:")
+    @terminal.puts("#{num1} - #{num2} = #{num1-num2}")
+  end
 end
--- codebases/mocking_bites/spec/interactive_calculator_spec.rb
+++ codebases/mocking_bites/spec/interactive_calculator_spec.rb
@@ -1,4 +1,16 @@
 require 'interactive_calculator'
 
 RSpec.describe InteractiveCalculator do
+  it "subtracts two numbers" do
+    fake_terminal = double :kernel 
+    calc = InteractiveCalculator.new(fake_terminal)
+    expect(fake_terminal).to receive(:puts).with("Hello. I will subtract two numbers").and_return(nil).ordered
+    expect(fake_terminal).to receive(:puts).with("Please enter a number").and_return(nil).ordered
+    expect(fake_terminal).to receive(:gets).and_return("4\n").ordered
+    expect(fake_terminal).to receive(:puts).with("Please enter another number").and_return(nil).ordered
+    expect(fake_terminal).to receive(:gets).and_return("3\n").ordered
+    expect(fake_terminal).to receive(:puts).with("Here is your result:").and_return(nil).ordered
+    expect(fake_terminal).to receive(:puts).with("4 - 3 = 1").and_return(nil).ordered
+    calc.run()
+  end
 end
